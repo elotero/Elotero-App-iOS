937c76c7f4a6f65b4c96ff9d859d1d7be9d83bd6
Chris Martinez <chrisumartinez@gmail.com>
2019-06-09 13:05:59 -0700

    Base Project Inclusions

---
+++ Elotero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Elotero
+//
+//  Created by Chris Martinez on 6/9/19.
+//  Copyright © 2019 Chris Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
